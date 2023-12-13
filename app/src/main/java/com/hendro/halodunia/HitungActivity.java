ea7b3017c5911e8ee55f73f54a4c293acde77fab
hendroprwk08 <40080703+hendroprwk08@users.noreply.github.com>
2023-12-13 13:40:36 +0700

    update

--- app/src/main/java/com/hendro/halodunia/HitungActivity.java
+++ app/src/main/java/com/hendro/halodunia/HitungActivity.java
@@ -1,12 +1,9 @@
 package com.hendro.halodunia;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-
 import com.hendro.halodunia.databinding.ActivityHitungBinding;
 
 public class HitungActivity extends AppCompatActivity {
@@ -24,58 +21,43 @@ public class HitungActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
 
-        binding.btTambah.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int nilai_1 = Integer.parseInt(binding.etNilai1.getText().toString());
-                int nilai_2 = Integer.parseInt(binding.etNilai2.getText().toString());
-                int hasil = nilai_1 + nilai_2;
+        binding.btTambah.setOnClickListener(view -> {
+            int nilai_1 = Integer.parseInt(binding.etNilai1.getText().toString());
+            int nilai_2 = Integer.parseInt(binding.etNilai2.getText().toString());
+            int hasil = nilai_1 + nilai_2;
 
-                binding.etHasil.setText(String.valueOf(hasil));
-            }
+            binding.etHasil.setText(String.valueOf(hasil));
         });
 
-        binding.btKurang.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int nilai_1 = Integer.parseInt(binding.etNilai1.getText().toString());
-                int nilai_2 = Integer.parseInt(binding.etNilai2.getText().toString());
-                int hasil = nilai_1 - nilai_2;
+        binding.btKurang.setOnClickListener(view -> {
+            int nilai_1 = Integer.parseInt(binding.etNilai1.getText().toString());
+            int nilai_2 = Integer.parseInt(binding.etNilai2.getText().toString());
+            int hasil = nilai_1 - nilai_2;
 
-                binding.etHasil.setText(String.valueOf(hasil));
-            }
+            binding.etHasil.setText(String.valueOf(hasil));
         });
 
-        binding.btKali.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int nilai_1 = Integer.parseInt(binding.etNilai1.getText().toString());
-                int nilai_2 = Integer.parseInt(binding.etNilai2.getText().toString());
-                int hasil = nilai_1 * nilai_2;
+        binding.btKali.setOnClickListener(view -> {
+            int nilai_1 = Integer.parseInt(binding.etNilai1.getText().toString());
+            int nilai_2 = Integer.parseInt(binding.etNilai2.getText().toString());
+            int hasil = nilai_1 * nilai_2;
 
-                binding.etHasil.setText(String.valueOf(hasil));
-            }
+            binding.etHasil.setText(String.valueOf(hasil));
         });
 
-        binding.btBagi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int nilai_1 = Integer.parseInt(binding.etNilai1.getText().toString());
-                int nilai_2 = Integer.parseInt(binding.etNilai2.getText().toString());
-                float hasil = nilai_1 / nilai_2;
+        binding.btBagi.setOnClickListener(view -> {
+            int nilai_1 = Integer.parseInt(binding.etNilai1.getText().toString());
+            int nilai_2 = Integer.parseInt(binding.etNilai2.getText().toString());
+            float hasil = (float) nilai_1 / nilai_2;
 
-                binding.etHasil.setText(String.valueOf(hasil));
-            }
+            binding.etHasil.setText(String.valueOf(hasil));
         });
 
-        binding.btSelesai.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent();
-                intent.putExtra("nilai_hasil", binding.etHasil.getText().toString());
-                setResult(Activity.RESULT_OK, intent);
-                finish();
-            }
+        binding.btSelesai.setOnClickListener(view -> {
+            Intent intent = new Intent();
+            intent.putExtra("nilai_hasil", binding.etHasil.getText().toString());
+            setResult(Activity.RESULT_OK, intent);
+            finish();
         });
     }
 }
--- app/src/main/java/com/hendro/halodunia/PesanActivity.java
+++ app/src/main/java/com/hendro/halodunia/PesanActivity.java
@@ -5,32 +5,25 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.view.View;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
-
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.snackbar.Snackbar;
 import com.hendro.halodunia.databinding.ActivityPesanBinding;
-
 import java.util.Objects;
 
 public class PesanActivity extends AppCompatActivity {
 
     private ActivityPesanBinding binding;
-    String CHANNEL_NOTIF = "channelku";
-    String CHANNEL_ID = "default";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,98 +45,77 @@ public class PesanActivity extends AppCompatActivity {
         binding.btSnack.setOnClickListener(view -> {
             Snackbar snackBar = Snackbar
                     .make(view, "Snack Bar!", Snackbar.LENGTH_LONG)
-                    .setAction("OK", new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            Toast.makeText(getApplicationContext(), "Snack Bar menutup", Toast.LENGTH_SHORT).show();
-                        }
-                    });
+                    .setAction("OK", view1 -> Toast.makeText(getApplicationContext(), "Snack Bar menutup", Toast.LENGTH_SHORT).show());
             snackBar.setActionTextColor(Color.BLUE);
             snackBar.show();
         });
 
-        binding.btDialog.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(PesanActivity.this);
-                alertDialogBuilder.setMessage("Ingin menutup Activity ini?");
+        binding.btDialog.setOnClickListener(view -> {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(PesanActivity.this);
+            alertDialogBuilder.setMessage("Ingin menutup Activity ini?");
 
-                alertDialogBuilder.setPositiveButton("Ya",
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface arg0, int arg1) {
-                                finish();
-                            }
-                        });
+            alertDialogBuilder.setPositiveButton("Ya",
+                    (arg0, arg1) -> finish());
 
-                alertDialogBuilder.setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                });
+            alertDialogBuilder.setNegativeButton("Tidak", (dialog, which) -> dialog.dismiss());
 
-                AlertDialog alertDialog = alertDialogBuilder.create();
-                alertDialog.show();
-            }
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
         });
 
-        binding.btNotifikasi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // apakah permission sudah diaktifkan pada ponsel?
-                if (NotificationManagerCompat.from(PesanActivity.this).areNotificationsEnabled()) {
-                    //notifikasi
-                    NotificationCompat.Builder mBuilder =
-                            new NotificationCompat.Builder(getApplicationContext(), "notify_001");
-                    Intent i = new Intent(getApplicationContext(), MainActivity.class);
-                    PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, i, PendingIntent.FLAG_IMMUTABLE);
+        binding.btNotifikasi.setOnClickListener(view -> {
+            // apakah permission sudah diaktifkan pada ponsel?
+            if (NotificationManagerCompat.from(PesanActivity.this).areNotificationsEnabled()) {
+                //notifikasi
+                NotificationCompat.Builder mBuilder =
+                        new NotificationCompat.Builder(getApplicationContext(), "notify_001");
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, i, PendingIntent.FLAG_IMMUTABLE);
 
-                    NotificationCompat.BigTextStyle bigText = new NotificationCompat.BigTextStyle();
-                    bigText.setBigContentTitle(getApplicationContext().getResources().getString(R.string.app_name));
-                    bigText.setSummaryText(getApplicationContext().getResources().getString(R.string.notifikasi));
+                NotificationCompat.BigTextStyle bigText = new NotificationCompat.BigTextStyle();
+                bigText.setBigContentTitle(getApplicationContext().getResources().getString(R.string.app_name));
+                bigText.setSummaryText(getApplicationContext().getResources().getString(R.string.notifikasi));
 
-                    mBuilder.setContentIntent(pendingIntent);
-                    mBuilder.setSmallIcon(R.mipmap.ic_launcher);
-                    mBuilder.setContentTitle(getApplicationContext().getResources().getString(R.string.app_name));
-                    mBuilder.setContentText(getApplicationContext().getResources().getString(R.string.notifikasi));
-                    mBuilder.setPriority(Notification.PRIORITY_MAX);
-                    mBuilder.setStyle(bigText);
-                    mBuilder.setDefaults(Notification.DEFAULT_SOUND); //suara
-                    mBuilder.setVibrate(new long[]{1000, 1000, 1000, 1000, 1000, 1000}); //getar
+                mBuilder.setContentIntent(pendingIntent);
+                mBuilder.setSmallIcon(R.mipmap.ic_launcher);
+                mBuilder.setContentTitle(getApplicationContext().getResources().getString(R.string.app_name));
+                mBuilder.setContentText(getApplicationContext().getResources().getString(R.string.notifikasi));
+                mBuilder.setPriority(Notification.PRIORITY_MAX);
+                mBuilder.setStyle(bigText);
+                mBuilder.setDefaults(Notification.DEFAULT_SOUND); //suara
+                mBuilder.setVibrate(new long[]{1000, 1000, 1000, 1000, 1000, 1000}); //getar
 
-                    NotificationManager mNotificationManager =
-                            (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+                NotificationManager mNotificationManager =
+                        (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
 
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                        NotificationChannel channel = new NotificationChannel("notify_001",
-                                "channelku",
-                                NotificationManager.IMPORTANCE_DEFAULT);
-                        mNotificationManager.createNotificationChannel(channel);
-                    }
-
-                    mNotificationManager.notify(0, mBuilder.build());
-                } else {
-                    new MaterialAlertDialogBuilder(PesanActivity.this)
-                            .setIcon(R.mipmap.ic_launcher)
-                            .setTitle("Perhatian")
-                            .setMessage("Ingin mengaktifkan Notifikasi?")
-                            .setCancelable(true)
-                            .setPositiveButton("Ya", (dialog, which) -> {
-                                // buka notification setting
-                                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                                Uri uri = Uri.fromParts("package", getPackageName(), null);
-                                intent.setData(uri);
-                                startActivity(intent);
-                            })
-                            .setNegativeButton("Tidak", (dialog, which) -> {
-
-                                    }
-                            )
-                            .show();
-
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    NotificationChannel channel = new NotificationChannel("notify_001",
+                            "channelku",
+                            NotificationManager.IMPORTANCE_DEFAULT);
+                    mNotificationManager.createNotificationChannel(channel);
                 }
+
+                mNotificationManager.notify(0, mBuilder.build());
+            } else {
+                new MaterialAlertDialogBuilder(PesanActivity.this)
+                        .setIcon(R.mipmap.ic_launcher)
+                        .setTitle("Perhatian")
+                        .setMessage("Ingin mengaktifkan Notifikasi?")
+                        .setCancelable(true)
+                        .setPositiveButton("Ya", (dialog, which) -> {
+                            // buka notification setting
+                            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            Uri uri = Uri.fromParts("package", getPackageName(), null);
+                            intent.setData(uri);
+                            startActivity(intent);
+                        })
+                        .setNegativeButton("Tidak", (dialog, which) -> {
+
+                                }
+                        )
+                        .show();
+
             }
         });
     }
